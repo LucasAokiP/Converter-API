bd16eb4bc8005c5510806a8f1575c1fac34a6804
Lucas Aoki <lucas.aoki2001@gmail.com>
2025-05-30 21:26:45 -0300

    criaçao da funçao get exchange rate

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 //Chave da API: 376f9e92ec2c3bc9c3ccf8f7
 const apiKey = "376f9e92ec2c3bc9c3ccf8f7";
 const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
+
+async function getExchangeRate(daMoeda,paraMoeda) {
+    try{
+        const response = await fetch(`${apiURL}${daMoeda}`);
+        const data = await response.json();
+
+        if(data.result === "success"){
+            return data.conversion_rates[paraMoeda];
+        }
+    }
+    catch{
+
+    }
+}
